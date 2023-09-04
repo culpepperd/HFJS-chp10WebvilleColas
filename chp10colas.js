ec93237ad130ae4553006c75a1a2cab5ea0a946d
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-03 22:54:18 -0400

    printing items

--- chp10colas.js
+++ chp10colas.js
@@ -1,3 +1,30 @@
-var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 }
+var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+                 { name: "Orange", calories: 160, color: "orange", sold: 12101 },
+                 { name: "Cola", calories: 210, color: "caramel", sold: 25412 },
+                 { name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+                 { name: "Lemon", calories: 200, color: "clear", sold: 14983 },
+                 { name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
+                 { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
+                 { name: "Water", calories: 0, color: "clear", sold: 62123 }
+               ];
 
-];
+/*
+var printAllColas = function (products) {
+    for ( var i = 0; i < products.length; i++ ) {
+        var item = products[i].name;
+        printThis("Name: " + item);
+        var item = products[i].calories;
+        printThis("Calories: " + item);
+        var item = products[i].color;
+        printThis("Color: " + item);
+        var item = products[i].sold;
+        printThis("Sold: " + item);
+    }
+}
+
+var printThis = function (item) {
+    console.log(item);
+}
+
+printAllColas(products);
+*/
